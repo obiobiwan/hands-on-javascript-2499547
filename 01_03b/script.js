6e99bc00c731e5e87c66f43666d70f8cb5df2b45
obiobiwan <83533768+obiobiwan@users.noreply.github.com>
2024-04-04 23:03:22 +0000

    Change card to cardlist

--- 01_03b/script.js
+++ 01_03b/script.js
@@ -6,7 +6,7 @@
  */
 
 import data from "./data.js";
-import Cardlist from "./components/Cardlist";
+import Cardlist from "./components/Cardlist.js";
 
 const mainContent = document.querySelector(".main-content");
 
